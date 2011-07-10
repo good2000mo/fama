eb5e773ae1bc49a9c546d22a05cd47ef93f744f7
Comus Leong <good2000mo@hotmail.com>
2011-07-10 15:47:04 +0800

    after install

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_type = 'mysqli';
+$db_host = 'localhost';
+$db_name = 'xman3';
+$db_username = 'xman3';
+$db_password = '23230647';
+$db_prefix = 'fbb_';
+$p_connect = false;
+
+$cookie_name = 'pun_cookie_05fa6e';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+$cookie_seed = '6446b1653ff17ca4';
+
+define('PUN', 1);
